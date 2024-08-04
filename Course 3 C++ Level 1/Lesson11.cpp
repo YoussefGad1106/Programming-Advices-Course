1119d7d180df97ae266518dbe5b7bdf2d0178812
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-05 02:58:35 +0300

    Add files via upload

---
+++ Course 3 C++ Level 1/Lesson11.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // Importing input/output stream library
+
+int main()  // Main function which is the entry point to the program
+{
+    std::cout<< "Youssef\n";
+    /*
+    * std --> standard library in the iostream library
+    * cout --> console out = print --> exists in the std
+    * Don't forget the ; and <<  (in case you didn't put << the program will print nothing)
+    * as the main function of the int type so it should return int so we return 0
+    */
+    
+    return 0; 
+}
+
