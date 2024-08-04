316283a008dc6a654f367f183b1a2cbc3ddc00f5
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-05 02:59:14 +0300

    Add files via upload

---
+++ Course 3 C++ Level 1/Lesson12_Homework.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main()
+{
+	// Question 1 :
+
+	/*  std::cout << "********************\n";
+		std::cout << "Name : Youssef Gad\n";
+		std::cout << "Age : 20\n";
+		std::cout << "City : Alexandria\n";
+		std::cout << "Country : Egypt\n";
+		std::cout << "********************\n";
+	*/
+
+	// Question 2 :
+
+	
+	/*	std::cout << "********************\n";
+		std::cout << "********************\n";
+		std::cout << "********************\n";
+		std::cout << "********************\n";	
+	*/
+	
+	// Question 3 : ** You can also use endl instead of \n
+		
+	/*  std::cout << "I love programming\n\n";
+		std::cout << "I promise to be the best developer ever!\n\n";
+		std::cout << "I know it will take some time to practice but I will achieve my goal\n\n";
+		std::cout << "Best Regrads,\n";
+		std::cout << "Youssef \n";
+	*/
+		
+	// Question 4 :
+		
+		
+	/*	std::cout << ".    .\n";
+		std::cout << ".    .\n";
+		std::cout << "......\n";
+		std::cout << ".    .\n";
+		std::cout << ".    .\n";
+	*/
+		
+
+
+
+	return 0;
+}
