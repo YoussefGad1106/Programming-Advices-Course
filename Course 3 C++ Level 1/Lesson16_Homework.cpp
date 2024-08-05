88268486bd61e065d4302fb05c55c05f3a09f392
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-05 03:00:42 +0300

    Add files via upload

---
+++ Course 3 C++ Level 1/Lesson16_Homework.cpp
@@ -0,0 +1,42 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Question 1 :
+    //cout << "\a";
+
+    // Question 2 :
+    /*  cout << "Dear Sir\\Madam,\n\n";
+        cout << "How are you?\n\n";
+        cout << "My name is \"Youssef Gad\", nice to meet you.\n\n";
+    */
+
+    // Question 3 :
+     /* cout << "Ali\tAhmed\tLina\n";
+        cout << "Fadi\tZain\tMona\n";
+     */ 
+
+    // Question 4 :
+    //  - Data used to represent a fixed value and can be used directly in the code .
+     
+    // Question 5 :
+    //  - No , As they only represent fixed values 
+
+    // Question 6 :
+    /*    1) Integer Literals : Decimal, Octal, Hexadecimal
+          2) Float Literals
+          3) Character Literals
+          4) Escape Sequences Literals .
+    */
+
+    // Question 7 :
+    // - To be able to change the format of the printed text and add some characters to it which give it a meaning
+
+         
+    return 0;
+
+}
+
