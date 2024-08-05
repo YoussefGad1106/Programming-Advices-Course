df74ec904d2a4b1e261fcf28edd598ef2c213d8a
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-05 03:00:10 +0300

    Add files via upload

---
+++ Course 3 C++ Level 1/Lesson15.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    /*
+        this code will
+        print the hello world text
+        and go to the next line
+    */
+
+    // this code will print hello world text
+    cout << "Hello World!\n";
+}
+
