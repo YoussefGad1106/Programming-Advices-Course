4c8411bed7646b59d156de87f9d2d4c727d967ec
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-06 21:20:27 +0300

    Update and rename Lesson12_Homework_2.cpp to Lesson13_Homework.cpp

--- Course 3 C++ Level 1/Lesson13_Homework.cpp
+++ Course 3 C++ Level 1/Lesson13_Homework.cpp
@@ -3,43 +3,43 @@ using namespace std;
 
 int main()
 {
-	// Question 1 :
+	 Question 1 :
 
-	/*  cout << "********************\n";
+	  cout << "********************\n";
 		cout << "Name : Youssef Gad\n";
 		cout << "Age : 20\n";
 		cout << "City : Alexandria\n";
 		cout << "Country : Egypt\n";
 		cout << "********************\n";
-	*/
+	
 
-	// Question 2 :
+	 Question 2 :
 
 
-	/*	cout << "********************\n";
 		cout << "********************\n";
 		cout << "********************\n";
 		cout << "********************\n";
-	*/
+		cout << "********************\n";
+	
 
-	// Question 3 : ** You can also use endl instead of \n
+	 Question 3 : ** You can also use endl instead of \n
 
-	/*  cout << "I love programming\n\n";
+	  cout << "I love programming\n\n";
 		cout << "I promise to be the best developer ever!\n\n";
 		cout << "I know it will take some time to practice but I will achieve my goal\n\n";
 		cout << "Best Regrads,\n";
 		cout << "Youssef \n";
-	*/
+	
 
-	// Question 4 :
+	 Question 4 :
 
 
-	/*	cout << ".    .\n";
+		cout << ".    .\n";
 		cout << ".    .\n";
 		cout << "......\n";
 		cout << ".    .\n";
 		cout << ".    .\n";
-	*/
+	
 
 
 
