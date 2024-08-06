8dd17fded9b42873bc72bf93d596a42a40a2ae14
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-06 21:21:25 +0300

    Update Lesson12_Homework.cpp

--- Course 3 C++ Level 1/Lesson12_Homework.cpp
+++ Course 3 C++ Level 1/Lesson12_Homework.cpp
@@ -2,43 +2,43 @@
 
 int main()
 {
-	// Question 1 :
+	 Question 1 :
 
-	/*  std::cout << "********************\n";
+	  std::cout << "********************\n";
 		std::cout << "Name : Youssef Gad\n";
 		std::cout << "Age : 20\n";
 		std::cout << "City : Alexandria\n";
 		std::cout << "Country : Egypt\n";
 		std::cout << "********************\n";
-	*/
+	
 
-	// Question 2 :
+	 Question 2 :
 
 	
-	/*	std::cout << "********************\n";
+		std::cout << "********************\n";
 		std::cout << "********************\n";
 		std::cout << "********************\n";
 		std::cout << "********************\n";	
-	*/
 	
-	// Question 3 : ** You can also use endl instead of \n
+	
+	 Question 3 : ** You can also use endl instead of \n
 		
-	/*  std::cout << "I love programming\n\n";
+	 	std::cout << "I love programming\n\n";
 		std::cout << "I promise to be the best developer ever!\n\n";
 		std::cout << "I know it will take some time to practice but I will achieve my goal\n\n";
 		std::cout << "Best Regrads,\n";
 		std::cout << "Youssef \n";
-	*/
+
 		
-	// Question 4 :
+	 Question 4 :
 		
 		
-	/*	std::cout << ".    .\n";
+		std::cout << ".    .\n";
 		std::cout << ".    .\n";
 		std::cout << "......\n";
 		std::cout << ".    .\n";
 		std::cout << ".    .\n";
-	*/
+	
 		
 
 
