26d31b36d075d7878fecbffc37bda7e9561fc1bf
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-07 04:24:09 +0300

    Add files via upload

---
+++ Course 3 C++ Level 1/Lesson20_Homework.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A , B ;
+
+    cout << "Please enter the first number : \n";
+    cin >> A;
+
+    cout << "Please enter the second number : \n";
+    cin >> B;
+
+    cout << "A + B = " << A + B << endl;
+    cout << "A - B = " << A - B << endl;
+    cout << "A * B = " << A * B << endl;
+    cout << "A / B = " << A / B << endl;
+    cout << "A % B = " << A % B << endl;
+}
+
