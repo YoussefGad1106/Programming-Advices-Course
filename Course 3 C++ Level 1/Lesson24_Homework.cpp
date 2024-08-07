eb44af88401662a342444e40c0e6b34f9878efce
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-07 04:24:29 +0300

    Add files via upload

---
+++ Course 3 C++ Level 1/Lesson24_Homework.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A , B ;
+
+	cout << "Please enter the first number A ? \n";
+	cin >> A;
+
+	cout << "Please enter the first number B ? \n";
+	cin >> B;
+	
+
+	cout << A <<" = "<< B << " is " << (A == B) << endl;
+
+	cout << A << " != " << B << " is " << (A!=B) << endl;
+
+	cout << A << " > " << B << " is " << (A>B) << endl;
+
+	cout << A << " < " << B << " is " << (A<B) << endl;
+	
+	cout << A << " >= " << B << " is " << (A>=B) << endl;
+
+	cout << A << " <= " << B << " is " << (A <= B) << endl;
+
+	
+
+	
+
+
+
+
+
+}
+
