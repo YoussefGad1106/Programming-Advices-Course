5c2f7bfd056c35a6ab1d5954c471dcc2db9f5bc4
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-07 04:23:37 +0300

    Add files via upload

---
+++ Course 3 C++ Level 1/Lesson17_Homework.cpp
@@ -0,0 +1,68 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	// Program 1
+//		
+//	    string Name = "Youssef Gad";
+//		int Age = 20;
+//		string City = "Alexandria";
+//		string Country = "Egypt";
+//		float MonthlySalary = 5000;
+//		char Gender = 'M';
+//		bool isMarried = false;
+//
+//		//float YearlySalary = MonthlySalary * 12; we don't have to make a variable for it
+//
+//		cout << "******************************\n";
+//		cout << "Name: " << Name << endl;
+//		cout << "Age: " << Age << endl;
+//		cout << "City: " << City << endl;
+//		cout << "Country: " << Country << endl;
+//		cout << "Mothly Salary: " << MonthlySalary << endl;
+//		cout << "Yearly Salary: " << MonthlySalary * 12 << endl;
+//		cout << "Gender: " << Gender << endl;
+//		cout << "Married: " << isMarried << endl;
+//		cout << "******************************\n";
+//  
+//
+//
+//	// Program 2
+//
+//		int num1 = 20 , num2 = 30 , num3 = 10;  // it is good to declare them on the same line
+//
+//		cout << num1 << " +\n" << num2 << " +\n" << num3 << "\n\n" << "----------------------------------------------\n\n" << "Total = " << num1 + num2 + num3;
+//
+//	
+//
+//	// Program 3
+//
+//		int Age = 25;
+//
+//		cout << "After 5 years you will be " << Age + 5 << " years old.\n";
+//
+//	
+//	
+//	
+//	// Variables names questions :
+//
+//		1- valid , as it consists of letters and doesnot violate any rule
+//		2- invalid , as we are only allowed to use the underscore special character
+//		3- valid , as we can use the underscore
+//		4- valid , as the name can start with a letter or an underscore
+//		5- invalid , as the name cannot start with a digit
+//		6- valid , as it starts with a letter
+//		7_ invalid , as it contains whitespace which violate the rules
+//		8- valid , as it doesnot contain whitespace
+//		9- invalid , as we are only allowed to use the underscore special character
+//		10- invalid , as we are only allowed to use the underscore special character
+//		11- invalid , as we are only allowed to use the underscore special character
+//		12- invalid , as we cannot use any reserved keywords
+//		13 - invalid, as we cannot use any reserved keywords
+//		
+//	
+//	
+//	
+//	return 0;
+//}
