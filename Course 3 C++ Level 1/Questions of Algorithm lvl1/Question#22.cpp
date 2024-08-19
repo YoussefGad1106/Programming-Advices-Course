6a726f9fa535c09fbcaedc8061093c76fd0e254a
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-19 21:54:48 +0300

    Update Question#22.cpp

--- Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#22.cpp
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#22.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 
+float circleArea(short equalSide ,short Base)
+{
+    return ((3.14 / 4) * pow(Base, 2)) * ((2 * equalSide) - Base) / ((2 * equalSide) + Base);
+}
+
 int main()
 {
-    int equalSide, Base;
+    short equalSide, Base;
 
     cout << "Enter the equalSide length : ";
     cin >> equalSide;
-    
+
     cout << "Enter the Base length : ";
     cin >> Base;
 
-    cout << ((3.14 / 4) * pow(Base , 2) ) * ((2 * equalSide) - Base) / ((2 * equalSide) + Base) << endl;
-
-
+    cout << "The area of the Circle is : " << circleArea(equalSide , Base) << endl;
 }
