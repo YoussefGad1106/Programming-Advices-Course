99008766ace7d2c07c9725975a3fed53240f3b33
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-19 21:53:12 +0300

    Update Question#18.cpp

--- Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#18.cpp
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#18.cpp
@@ -2,15 +2,18 @@
 #include <cmath>
 using namespace std;
 
+float circleArea(float radius)
+{
+	return 3.14 * pow(radius, 2);
+}
+
 int main()
 {
-
-	short radius;
+	float radius;
 
 	cout << "Enter the radius length : ";
 	cin >> radius;
 
-	cout << "The area = " << 3.14 * pow(radius , 2) << endl ;
-
-
+	cout << "The area of the Circle is : " << circleArea(radius) << endl;
 }
+	
