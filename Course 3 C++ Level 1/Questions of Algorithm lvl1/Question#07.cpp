a700fa2bf5e031a60612ed12d7b34bce7221ce76
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-24 16:15:02 +0300

    Update Question#07.cpp

--- Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#07.cpp
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#07.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int ReadNumber()
 {
     int number;
 
     cout << "Please enter a number to get its half : ";
-    cin >> number ;
-    //cout << endl;
+    cin >> number;
 
-    cout << "Half of " << number << " is " << number / 2 << endl;
+    return number;
+}
+
+float GetHalf(int num)
+{
+    return num / 2.0;
+}
+
+void PrintHalf(int Number)
+{
+    cout << "Half of " << Number << " is " << GetHalf(Number) << endl;
+}
+
+int main()
+{
+    PrintHalf(ReadNumber());
 }
