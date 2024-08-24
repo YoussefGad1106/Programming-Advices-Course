f158337168ed44cc9f279f503f04364d0e8f1667
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-24 19:09:18 +0300

    Update Question#10.cpp

--- Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#10.cpp
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#10.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 using namespace std;
 
+struct stNumbers
+{
+    int Num1, Num2, Num3;
+};
+
+stNumbers ReadNumbers()
+{
+    stNumbers Nums;
+   
+    cout << "Enter the First Number : ";
+    cin >> Nums.Num1;
+
+    cout << "Enter the Second Number : ";
+    cin >> Nums.Num2;
+
+    cout << "Enter the Third Number : ";
+    cin >> Nums.Num3;
+
+    return Nums;
+}
+
+int Sum3Numbers(stNumbers Nums)
+{
+    return Nums.Num1 + Nums.Num2 + Nums.Num3;
+}
+
+void PrintAverage(int Sum)
+{
+    cout << "\nThe Average is " << Sum/3.0 << endl;
+}
 
 int main()
 {
-    int Mark1, Mark2, Mark3;
-
-    cout << "Enter the First Mark : ";
-    cin >> Mark1;
-
-    cout << "Enter the Second Mark : ";
-    cin >> Mark2;
-
-    cout << "Enter the Third Mark : ";
-    cin >> Mark3;
-
-
-    cout << (Mark1 + Mark2 + Mark3 ) / 3 << endl;
-
-
+    PrintAverage(Sum3Numbers(ReadNumbers()));
 }
