75c7b19559e75bad4e9e330ff2147f0fc111c94c
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-25 12:26:40 +0300

    Update Question#17.cpp

--- Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#17.cpp
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#17.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+void ReadDimensions(int& base, int& height)
+{
+	cout << "Enter the base length : ";
+	cin >> base;
+
+	cout << "Enter the height length : ";
+	cin >> height;
+}
+
+float CalculateTriangleArea(int base, int height)
+{
+	return 0.5 * base * height;
+}
+
+void PrintArea(float Area)
+{
+	cout << "\nThe Area of the Triangle is : " << Area << endl;
+}
 
 
 int main()
 {
-    int base, height;
-
-    cout << "Enter the base length : ";
-    cin >> base;
-
-    cout << "Enter the height length : ";
-    cin >> height;
-
-    cout << (0.5 * base * height) << endl;
-
+	int base, height;
+	
+	ReadDimensions(base, height);
+	PrintArea(CalculateTriangleArea(base, height));
 
 }
