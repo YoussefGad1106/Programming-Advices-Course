f618f2468e21551a1a94822d0e163aa7b0b4e947
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-25 12:43:43 +0300

    Create Question#18.cpp

---
+++ Course 4 Solving level 1 using C++/Question#18.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void ReadRadius(float& Radius)
+{
+	cout << "Enter the radius length : ";
+	cin >> Radius;
+}
+
+float CircleArea(float radius)
+{
+	return 3.14 * pow(radius, 2);
+}
+
+void PrintArea(float Area)
+{
+	cout << "\nThe Area of the Circle is : " << Area << endl;
+}
+
+int main()
+{
+	float radius;
+
+	ReadRadius(radius);
+	PrintArea(CircleArea(radius));
+}
