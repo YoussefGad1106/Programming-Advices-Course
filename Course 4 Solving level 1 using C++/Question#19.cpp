149ae5ab229e0245327457ebfb8f9afca900deef
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-25 12:44:03 +0300

    Create Question#19.cpp

---
+++ Course 4 Solving level 1 using C++/Question#19.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void ReadRadius(float& Diameter)
+{
+	cout << "Enter the Diameter length : ";
+	cin >> Diameter;
+}
+
+float CircleArea(float Diameter)
+{
+	return ((3.14 / 4) * pow(Diameter, 2));
+}
+
+void PrintArea(float Area)
+{
+	cout << "\nThe Area of the Circle is : " << Area << endl;
+}
+
+int main()
+{
+	float Diameter;
+
+	ReadRadius(Diameter);
+	PrintArea(CircleArea(Diameter));
+}
