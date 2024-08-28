2dfadd473711c91909c003e03138198a5fdcde9f
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 12:40:24 +0300

    Create Question#43.cpp

--- Course 4 Solving level 1 using C++/Question#43.cpp
+++ Course 4 Solving level 1 using C++/Question#43.cpp
@@ -1,1 +1,35 @@
 
+#include <iostream>
+using namespace std;
+
+
+void Converter(int numberOfSeconds)
+{
+    short Days, Hours, Minutes;
+
+    Days = numberOfSeconds / 24 / 3600;
+
+    numberOfSeconds -= Days * 24 * 3600;
+
+    Hours = numberOfSeconds / 3600;
+
+    numberOfSeconds -= Hours * 3600;
+
+    Minutes = numberOfSeconds / 60;
+
+    numberOfSeconds -= Minutes * 60;
+
+    cout << Days << ":" << Hours << ":" << Minutes << ":" << numberOfSeconds << endl;
+
+}
+
+int main()
+{
+    int numberOfSeconds;
+
+    cout << "Enter the total number of seconds : ";
+    cin >> numberOfSeconds;
+
+    Converter(numberOfSeconds);
+}
+
