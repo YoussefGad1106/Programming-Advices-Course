d176d9c4aebbb5852258da00d3d23a3b97a9c978
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 12:39:51 +0300

    Create Question#42.cpp

---
+++ Course 4 Solving level 1 using C++/Question#42.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+using namespace std;
+
+
+struct stTimes
+{
+    short Days, Hours, Minutes, Seconds;
+};
+
+stTimes ReadTime()
+{
+    stTimes time;
+
+    cout << "Enter the number of days : ";
+    cin >> time.Days;
+
+    cout << "Enter the number of hours : ";
+    cin >> time.Hours;
+
+    cout << "Enter the number of minutes : ";
+    cin >> time.Minutes;
+
+    cout << "Enter the number of seconds  : ";
+    cin >> time.Seconds;
+
+    return time;
+}
+
+float taskDuration(stTimes time)
+{
+    return (float)(time.Days * 24 * 3600) + (time.Hours * 3600) + (time.Minutes * 60) + time.Seconds;
+}
+int main()
+{
+     cout << "The Task Duration is : " << taskDuration(ReadTime()) << endl;
+}
