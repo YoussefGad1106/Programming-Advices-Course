f82becaa58deea31aff727b1265c4873bb74da77
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 05:45:53 +0300

    Update Question#23.cpp

--- Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#23.cpp
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#23.cpp
@@ -2,18 +2,8 @@
 #include <cmath>
 using namespace std;
 
-float circleArea(short side1, short side2, short side3)
+void ReadSides(int& side1, int& side2, int& side3)
 {
-	float p;
-	p = (side1 + side2 + side3) / 2;
-
-	return 3.14 * pow((side1 * side2 * side3) / (4 * sqrt(p * (p - side1) * (p - side2) * (p - side3))), 2);
-		
-}
-int main()
-{
-	short side1, side2, side3;
-	
 	cout << "Enter the length of the first side : ";
 	cin >> side1;
 
@@ -22,6 +12,25 @@ int main()
 
 	cout << "Enter the length of the third side  : ";
 	cin >> side3;
-	
-	cout << "The area of the Circle is : " << circleArea(side1, side2, side3) << endl;
+}
+
+float CircleArea(int side1, int side2, int side3)
+{
+	float p;
+	p = (side1 + side2 + side3) / 2;
+
+	return 3.14 * pow((side1 * side2 * side3) / (4 * sqrt(p * (p - side1) * (p - side2) * (p - side3))), 2);
+}
+
+void PrintArea(float Area)
+{
+	cout << "\nThe Area of the Circle is : " << Area << endl;
+}
+
+int main()
+{
+	int side1, side2, side3;
+
+	ReadSides(side1, side2, side3);
+	PrintArea(CircleArea(side1, side2, side3));
 }
