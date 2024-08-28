c2954502388384f477a360bcbbbad57ab9378577
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 12:28:28 +0300

    Update Question#47.cpp

--- Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#47.cpp
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#47.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int ReadPositiveNumber(string Message)
+{
+    int Number;
+
+    cout << Message << endl;
+    cin >> Number;
+    
+    return Number;
+}
+
 
 int main()
 {
-    short LoanAmount, MonthlyPayment;
-
-    cout << " Please enter the loan amount : ";
-    cin >> LoanAmount;
-
-    cout << " please enter the monthly payment : ";
-    cin >> MonthlyPayment;
+    int LoanAmount = ReadPositiveNumber("Please enter the loan amount : ");
+    int MonthlyPayment = ReadPositiveNumber("please enter the monthly payment : ");
 
     cout << LoanAmount / MonthlyPayment << " Months\n";
-
-   
 
 }
