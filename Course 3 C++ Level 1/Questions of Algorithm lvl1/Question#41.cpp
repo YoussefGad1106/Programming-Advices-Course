cca48902552b2a90ffc63c94dd0604413035da2b
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 11:30:57 +0300

    Create Question#41.cpp

---
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#41.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+float ReadPositiveNumber(string Message)
+{
+	int Number;
+
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+
+	} while (Number <= 0);
+
+	return Number;
+}
+
+float HoursToDays(float NumberOfHours)
+{
+	return NumberOfHours / 24.0;
+}
+
+float HoursToWeeks(float NumberOfHours)
+{
+	return NumberOfHours / 24.0 / 7.0;
+}
+
+float DaysToWeeks(float NumberOfDays)
+{
+	return NumberOfDays / 7.0;
+}
+
+int main()
+{
+	float NumberOfHours = ReadPositiveNumber("Please Enter Number of Hours : ");
+	float NumberOfDays = HoursToDays(NumberOfHours);
+	float NumberOfWeeks = DaysToWeeks(NumberOfDays);
+
+	cout << "\nTotal Hours = " << NumberOfHours << endl;
+	cout << "\nTotal Days = " << NumberOfDays << endl;
+	cout << "\nTotal Weeks = " << NumberOfWeeks << endl;
+}
