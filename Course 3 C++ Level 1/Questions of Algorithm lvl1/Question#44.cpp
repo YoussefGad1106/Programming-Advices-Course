730df33e371dc9b2934247fd98539e11e0502d57
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 12:02:45 +0300

    Update Question#44.cpp

--- Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#44.cpp
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#44.cpp
@@ -1,62 +1,67 @@
+
 #include <iostream>
 using namespace std;
 
-short ReadDay()
+enum enDays
 {
-	short Number;
+	Sun = 1 , Mon = 2 , Tue = 3 , Wed = 4 ,Thu = 5 , Fri = 6 , Sat = 7 
+};
+
+void DisplayMenu()
+{
 
 	cout << "\n**********************************\n";
 	cout << " 1 Print Sunday\n";
 	cout << " 2 Print Monday\n";
 	cout << " 3 Print Tuesday\n";
 	cout << " 4 Print Wednesday\n";
-	cout << " 5 Print Tuesday\n";
+	cout << " 5 Print Thursdayy\n";
 	cout << " 6 Print Friday\n";
 	cout << " 7 Print Saturday\n";
 	cout << "\n**********************************\n";
-	cout << "Please Enter a Number : " << endl;
-	cin >> Number;
+}
+
+int ReadDay()
+{
+	int Number = 0;
+
+	do
+	{
+		cout << "Please Enter a Number : " << endl;
+		cin >> Number;
+
+	} while (Number <= 0 || Number > 7);
+	
 
 	return Number;
 }
-void printDay(short Number)
+
+enDays CheckDay(int Number)
 {
-	switch (Number)
-	{
-	case 1 :
-		cout << "Its Sunday" << endl;
-		break;
+	return (enDays)Number;
 
-	case 2 :
-		cout << "Its Monday" << endl;
-		break;
-
-	case 3 :
-		cout << "Its Tuesday" << endl;
-		break;
-
-	case 4 :
-		cout << "Its Wednesday" << endl;
-		break;
-
-	case 5 :
-		cout << "Its Tuesday" << endl;
-		break;
-
-	case 6 :
-		cout << "Its Friday" << endl;
-		break;
-
-	case 7 :
-		cout << "Its Saturday" << endl;
-		break;
-	
-	default:
-		cout << "Invalid Input";
-		break;
-	}
 }
+
+void PrintDay(enDays Day)
+{
+	if (Day == enDays::Sat)
+		cout << "Its Saturday" << endl;
+	else if (Day == enDays::Fri)
+		cout << "Its Friday" << endl;
+	else if (Day == enDays::Mon)
+		cout << "Its Monday" << endl;
+	else if (Day == enDays::Wed)
+		cout << "Its Wednesday" << endl;
+	else if (Day == enDays::Tue)
+		cout << "Its Tuesday" << endl;
+	else if (Day == enDays::Thu)
+		cout << "Its Thursday" << endl;
+	else
+		cout << "Its Sunday" << endl;
+}
+
 int main()
 {
-	printDay(ReadDay());
+	DisplayMenu();
+	PrintDay(CheckDay(ReadDay()));
 }
