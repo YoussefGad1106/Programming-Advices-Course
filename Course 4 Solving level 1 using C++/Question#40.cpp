744a865eb2477f17c25dcb8ac3c3b22e42200d2c
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 09:25:49 +0300

    Create Question#40.cpp

---
+++ Course 4 Solving level 1 using C++/Question#40.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int ReadBillValue()
+{
+    int BillValue;
+    
+    cout << "Enter the BillValue : ";
+    cin >> BillValue;
+
+    return BillValue;
+}
+
+float CalculateServices(int BillValue)
+{
+    float BillAfterService;
+
+    BillAfterService = (BillValue * 10 / 100) + BillValue;
+
+    return BillAfterService;
+}
+
+float CalculateTaxAndGetTotalBill (float BillAfterService)
+{
+    float TotalBill;
+    
+    TotalBill = (BillAfterService * 16 / 100) + BillAfterService;
+
+    return TotalBill;
+}
+
+
+void PrintTotalBill(float Total)
+{
+    cout << "The Total Bill is " << Total << endl;
+}
+
+int main()
+{
+    int N = ReadBillValue();
+    
+    PrintTotalBill(CalculateTaxAndGetTotalBill(CalculateServices(N)));
+
+}
