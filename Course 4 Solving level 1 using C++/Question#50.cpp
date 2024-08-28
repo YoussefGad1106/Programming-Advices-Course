9d6737f37787727ab5fa6e247f42a804fef7d800
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 12:42:36 +0300

    Create Question#50.cpp

---
+++ Course 4 Solving level 1 using C++/Question#50.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string ReadPIN()
+{
+	string PIN;
+
+	cout << "Please Enter your PIN : \n";
+	cin >> PIN;
+
+	return PIN;
+}
+
+void CheckPIN(string PIN)
+{
+	string PINCode = "1234";
+	short Attempts = 2;
+
+	while (PIN != PINCode && Attempts != 0)
+	{
+		system("color 4f");
+		cout << "Wrong PIN , You have " << Attempts << "more tries left\n";
+		PIN = ReadPIN();
+		Attempts--;
+	}
+
+	if (PIN == PINCode)
+	{
+		system("color 2f");
+		cout << " Your Balance is: 7500\n";
+	}
+
+	if (Attempts == 0)
+	{
+		cout << "Card is Locked\n";
+	}
+
+}
+
+int main()
+{
+	CheckPIN(ReadPIN());
+}
