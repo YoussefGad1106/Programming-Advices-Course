1cae0b927674506ee271ef782d79a739d04683e7
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 12:42:03 +0300

    Create Question#48.cpp

---
+++ Course 4 Solving level 1 using C++/Question#48.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+    int Number;
+
+    cout << Message << endl;
+    cin >> Number;
+    
+    return Number;
+}
+
+
+int main()
+{
+    int LoanAmount = ReadPositiveNumber("Please enter the loan amount : ");
+    int howManyMonths = ReadPositiveNumber("please enter the monthly payment : ");
+
+    cout << float(LoanAmount / howManyMonths) << endl;
+
+}
