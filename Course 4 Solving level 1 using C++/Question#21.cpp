70b0588a789d487aa57a7684fc4f0cf0167341ba
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 06:50:50 +0300

    Create Question#21.cpp

---
+++ Course 4 Solving level 1 using C++/Question#21.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void ReadCircumference(float& Circumference)
+{
+	cout << "Enter the Circumference value : ";
+	cin >> Circumference;
+}
+
+float CircleArea(float Circumference)
+{
+	return (pow(Circumference, 2) / (4 * 3.14));
+}
+
+void PrintArea(float Area)
+{
+	cout << "\nThe Area of the Circle is : " << Area << endl;
+}
+
+int main()
+{
+	float Circumference;
+
+	ReadCircumference(Circumference);
+	PrintArea(CircleArea(Circumference));
+}
+
