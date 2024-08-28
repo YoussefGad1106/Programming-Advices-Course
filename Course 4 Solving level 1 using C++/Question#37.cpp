1883a93f24c0a46f2e16dece5631e505f0401baa
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 09:25:03 +0300

    Create Question#37.cpp

---
+++ Course 4 Solving level 1 using C++/Question#37.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+
+int ReadNumbersAndSumThem()
+{
+	int sum = 0, Number = 0;
+
+	do
+	{
+		sum += Number;
+
+		cout << "Please Enter a Number or Enter (-99) to Exit\n";
+		cin >> Number;
+
+	} while (Number != -99);
+
+	return sum;
+}
+
+int main()
+{
+	cout << "The Sum is  " << ReadNumbersAndSumThem() << endl;
+}
