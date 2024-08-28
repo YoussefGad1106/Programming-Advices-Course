155ad240536a5320e4640f29ed4f5b1773c76dc7
YoussefGad1106 <167649131+YoussefGad1106@users.noreply.github.com>
2024-08-28 06:36:50 +0300

    Update Question#28.cpp

--- Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#28.cpp
+++ Course 3 C++ Level 1/Questions of Algorithm lvl1/Question#28.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 
@@ -12,7 +13,7 @@ int ReadNumber()
 	return Number;
 }
 
-int PrintSum(int N)
+int GetOddSum(int N)
 {
 	int sum = 0;
 
@@ -27,7 +28,12 @@ int PrintSum(int N)
 	return sum;
 }
 
+void PrintSum(int sum)
+{
+	cout << "The sum of the odd numbers is " << sum << endl;
+}
+
 int main()
 {
-	cout << "The sum of the odd numbers is " << PrintSum(ReadNumber()) << endl;
+	PrintSum(GetOddSum(ReadNumber()));
 }
